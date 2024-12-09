d2cbb1c4d4093b5e1ce9123b79ed4b3d33127d39
UsuarioGithub <oscivanov@gmail.com>
2024-12-09 17:20:32 -0500

    Primer Commit

---
+++ src/uniandes/dpoo/swing/interfaz/principal/PanelBotones.java
@@ -0,0 +1,50 @@
+package uniandes.dpoo.swing.interfaz.principal;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import uniandes.dpoo.swing.interfaz.principal.*;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class PanelBotones extends JPanel implements ActionListener
+{
+    private static final String NUEVO = "nuevo";
+    private static final String VER = "ver";
+
+    private JButton butNuevo;
+    private JButton butVerTodos;
+    private VentanaPrincipal ventanaPrincipal;
+
+    public PanelBotones( VentanaPrincipal ventanaPrincipal )
+    {
+        this.ventanaPrincipal = ventanaPrincipal;
+        setLayout( new FlowLayout( ) );
+
+        butNuevo = new JButton("Nuevo");
+        butNuevo.setActionCommand(NUEVO);
+        butNuevo.addActionListener(this);
+        add(butNuevo);
+
+        butVerTodos = new JButton("Ver Todos");
+        butVerTodos.setActionCommand(VER);
+        butVerTodos.addActionListener(this);
+        add(butVerTodos);
+    }
+
+    @Override
+    public void actionPerformed( ActionEvent e )
+    {
+        String comando = e.getActionCommand( );
+        if( comando.equals( NUEVO ) )
+        {
+            ventanaPrincipal.mostrarVetanaNuevoRestaurante( );
+        }
+        else if( comando.equals( VER ) )
+        {
+            ventanaPrincipal.mostrarVentanaMapa( );
+        }
+    }
+}
+
